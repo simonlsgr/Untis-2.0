db67672a9e638fb753219dfeef38c1915a1aae26
simonlsr <89278229+simonlsr@users.noreply.github.com>
2023-01-27 17:53:02 +0100

    generalize requests

--- python_tests/flask_test.py
+++ python_tests/flask_test.py
@@ -5,13 +5,6 @@ import sys
 app = flask.Flask(__name__)
 
 
-with open("test_output.json", "r") as f:
-    data = json.load(f)
-with open("test.json", "r") as f:
-    subjects = json.load(f)
-    
-subjects = subjects["data"]["result"]["data"]["elements"]
-
 def periods_in_a_week(timetable_formatted):
     list_of_periods = []
     for i, j in enumerate(timetable_formatted):
@@ -24,42 +17,78 @@ def periods_in_a_week(timetable_formatted):
     list_of_periods.sort()
     return list_of_periods
 
+def generate_foreColor(hex_color: str):
+    hex_color = hex_color.replace("#", "")
+    print(hex_color, file=sys.stderr)
 
-@app.route("/", methods=["GET", "POST"])
-def index():
+    r, g, b = [int(hex_color[i:i+2], 16) for i in (0, 2, 4)]
+
+    r = r*0.299
+    g = g*0.587
+    b = b*0.114
+    Y = r + g + b
+
+    if Y > 186:
+        return "#1d1f2b"
+    else:
+        return "#ffffff"
     
 
-    list_of_periods = periods_in_a_week(data)
+
+
+@app.route("/")
+def index():
+    with open("src/timetable.json", "r") as f:
+        timetable_input = json.load(f)
+    
+    if timetable_input:
+        return str(timetable_input)
+    else:
+        return str(timetable_input)
+    
+
+@app.route("/selecting_subjects", methods=["GET", "POST"])
+def selecting_subjects():
+    
+    with open("test_output_187.json", "r") as f:
+        KaiFU_data = json.load(f)
+    with open("test_187.json", "r") as f:
+        subjects = json.load(f)
+        
+    subjects = subjects["data"]["result"]["data"]["elements"]
+    
+
+    list_of_periods = periods_in_a_week(KaiFU_data)
     
     timetable_html_element = ""
     
     
-    timetable_html_element += f"""<form action="/selected_subjects" class="timetable_wrapper_form" method="POST">"""
+    timetable_html_element += f"""<form action="/personalized_timetable" class="timetable_wrapper_form" method="POST">"""
     timetable_html_element += f"""<div class="timetable_wrapper">"""
     
 
     weekdays = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"]
     timetable_html_element += f"""<div class="weekday"></div>"""
-    for index_tag, tag in enumerate(data):
+    for index_tag, tag in enumerate(KaiFU_data):
         timetable_html_element += f"""<div class="weekday">{weekdays[index_tag]}</div>"""
         
         
-    for index_stunde in range(len(data[0])):
+    for index_stunde in range(len(KaiFU_data[0])):
         timetable_html_element += f"""<div class="period"><div class="period_time">{list_of_periods[index_stunde][0]}-{list_of_periods[index_stunde][1]}</div></div>"""
-        for index_tag, tag in enumerate(data):
+        for index_tag, tag in enumerate(KaiFU_data):
             timetable_html_element += f"""<div class="period"><div class="period_time">{index_stunde}</div>"""
             for index_fach, fach in enumerate(tag[index_stunde]):
                 for index_element, element in enumerate(fach["elements"]):
                     for index_fach_informationen, fach_informationen in enumerate(subjects):
                         if fach_informationen["id"] == element["id"]:
                             
-                            if index_element == 0:
+                            if element["type"] == 3:
                                 fach_name = fach_informationen["name"]
                                 fach_lang_name = fach_informationen["longName"]
                                 
                                 fach_html_element = f"""<div class="subject_name"><div class="subject_short_name">{fach_name}</div><div class="subject_long_name">{fach_lang_name}</div></div>"""
                                 
-                            elif index_element == 1:
+                            elif element["type"] == 4:
                                 raum_name = fach_informationen["name"]
                                 raum_lang_name = fach_informationen["longName"]
                                 if raum_name == raum_lang_name:
@@ -92,11 +121,19 @@ def index():
     return html
 
 
-@app.route("/selected_subjects", methods=["GET", "POST"])
+@app.route("/personalized_timetable", methods=["GET", "POST"])
 def selected_subjects():
+    with open("test_output_187.json", "r") as f:
+        KaiFU_data = json.load(f)
+    with open("test_187.json", "r") as f:
+        subjects = json.load(f)
+        
+    subjects = subjects["data"]["result"]["data"]["elements"]
+    
+    
     list_of_subjects_id = []
     
-    for i, j in enumerate(data):
+    for i, j in enumerate(KaiFU_data):
         for k, l in enumerate(j):
             for m, n in enumerate(l):
                 if n["id"] not in list_of_subjects_id:
@@ -104,51 +141,66 @@ def selected_subjects():
     
     
     selected_subjects_lesson_id = []
+    blocked_ids = []
     if flask.request.method == "POST":
         for i, j in enumerate(list_of_subjects_id):
             if flask.request.form.get(str(j)):
-                for k, l in enumerate(data):
+                for k, l in enumerate(KaiFU_data):
                     for m, n in enumerate(l):
                         for o, p in enumerate(n):
                             if p["id"] == j:
                                 selected_subjects_lesson_id.append(p["lessonId"])
+        for i, j in enumerate(KaiFU_data):
+            for k, l in enumerate(j):
+                for m, n in enumerate(l):
+                    if n["lessonId"] not in selected_subjects_lesson_id:
+                        blocked_ids.append(n["lessonId"])
+            
     
     
     
     
     
-    list_of_periods = periods_in_a_week(data)
+    list_of_periods = periods_in_a_week(KaiFU_data)
     
     timetable_html_element = ""
     
     
-    timetable_html_element += f"""<form action="/selected_subjects" class="timetable_wrapper_form" method="POST">"""
     timetable_html_element += f"""<div class="timetable_wrapper">"""
     
 
     weekdays = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"]
     timetable_html_element += f"""<div class="weekday"></div>"""
-    for index_tag, tag in enumerate(data):
+    for index_tag, tag in enumerate(KaiFU_data):
         timetable_html_element += f"""<div class="weekday">{weekdays[index_tag]}</div>"""
         
         
-    for index_stunde in range(len(data[0])):
+    for index_stunde in range(len(KaiFU_data[0])):
         timetable_html_element += f"""<div class="period"><div class="period_time">{list_of_periods[index_stunde][0]}-{list_of_periods[index_stunde][1]}</div></div>"""
-        for index_tag, tag in enumerate(data):
+        for index_tag, tag in enumerate(KaiFU_data):
             timetable_html_element += f"""<div class="period"><div class="period_time"></div>"""
             for index_fach, fach in enumerate(tag[index_stunde]):
-                if fach["lessonId"] in selected_subjects_lesson_id:
+                if fach["lessonId"] not in blocked_ids:
                     for index_element, element in enumerate(fach["elements"]):
                         for index_fach_informationen, fach_informationen in enumerate(subjects):
                             if fach_informationen["id"] == element["id"]:
                                 
-                                if index_element == 0:
+                                
+                                
+                                if element["type"] == 3:
+                                    try: 
+                                        backColor = fach_informationen["backColor"]
+                                        foreColor = generate_foreColor(backColor)
+                                    except KeyError: 
+                                        backColor = "#1d1f2b"
+                                        foreColor = "#F3DFC1"
+                                        
                                     fach_name = fach_informationen["name"]
                                     fach_lang_name = fach_informationen["longName"]
                                     
                                     fach_html_element = f"""<div class="subject_name"><div class="subject_short_name">{fach_name}</div><div class="subject_long_name">{fach_lang_name}</div></div>"""
                                     
-                                elif index_element == 1:
+                                elif element["type"] == 4:
                                     raum_name = fach_informationen["name"]
                                     raum_lang_name = fach_informationen["longName"]
                                     if raum_name == raum_lang_name:
@@ -156,12 +208,29 @@ def selected_subjects():
                                     elif raum_name != raum_lang_name:
                                         raum_html_element = f"""<div class="room_name">{raum_name} ({raum_lang_name})</div>"""
                     id_for_checkbox = fach["id"]
-                    timetable_html_element += f"""<input type="checkbox" name="{id_for_checkbox}" value="{id_for_checkbox}" id="checkbox_number_{id_for_checkbox}"><label for="checkbox_number_{id_for_checkbox}" class="subject"><div class="info">{fach_html_element}{raum_html_element}</div></label>"""            
+                    timetable_html_element += f"""<label class="subject" style="background-color: {backColor}; color: {foreColor}"><div class="info">{fach_html_element}{raum_html_element}</div></label>"""            
             timetable_html_element += "</div>"
         
     timetable_html_element += "</div>"
-    timetable_html_element += """<input type="submit" value="Submit">"""
-    timetable_html_element += "</form>"
+    
+    
+    timetable_html_element += f"""<div class="wrapper">"""
+    for i, j in enumerate(subjects):
+        try:
+            if j["type"] == 3:
+                try:
+                    back = j["backColor"]
+                except KeyError:
+                    back = "#1d1f2b"
+                try:
+                    fore = generate_foreColor(back)
+                except KeyError:
+                    fore = "#F3DFC1"
+                timetable_html_element += f"""<div style="background-color: {back}; padding: 30px;"><p style="color: {fore};">{j["longName"]}</p></div>"""
+        except KeyError:
+            print(j)
+    timetable_html_element += f"""</div>"""
+        
             
             
     html = f"""    
--- python_tests/test.py
+++ python_tests/test.py
@@ -6,105 +6,110 @@ import itertools
 
 ### WEBUNTIS REQUEST ###
 
-url = "https://ikarus.webuntis.com/WebUntis/api/public/timetable/weekly/data"
 
-querystring = {"elementType":"1","elementId":"187","date":"2023-01-30"}
+class WebUntis_request:
+    def __init__(self, element_type, element_id, date, schoolname):
+        self.element_type = str(element_type)
+        self.element_id = str(element_id)
+        self.date = str(date)
+        self.schoolname = str(schoolname)
+        
+    def API_call(self) -> None:
+        
+        
+        url_get_cookies = f'https://ikarus.webuntis.com/WebUntis/?school={self.schoolname}#/basic/timetable'
 
-payload = ""
-headers = {
-    "cookie": "schoolname=%22_aGg1ODQ2%22; traceId=dbb69fa1e2dc8af353694414c2035671990c360d; JSESSIONID=4A9A47A4A726D2C56A3464F58F4AC75C",
-    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0",
-    "Accept": "application/json",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Referer": "https://ikarus.webuntis.com/WebUntis/?school=hh5846",
-    "DNT": "1",
-    "Connection": "keep-alive",
-    "Cookie": "traceId=e09d53487ed6066b8abffd29aed5cc75a9a1a4b0; schoolname=\"_aGg1ODQ2\"; schoolname=\"_aGg1ODQ2\"; JSESSIONID=B1E7E1D372B14ECCFEE5474DC920600E; schoolname=\"_aGg1ODQ2\"; traceId=e09d53487ed6066b8abffd29aed5cc75a9a1a4b0",
-    "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Site": "same-origin"
-}
+        response = requests.get(url_get_cookies)
 
-response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+        jsessionid_cookies = response.headers['Set-Cookie'].split(';')[0].split('=')[1]
 
-with open("python_tests/test.json", "w") as f:
-    f.write(response.text)
+        schoolname_cookies = response.headers['Set-Cookie'].split(';')[4].split('=')[2]
 
-###
+        url = "https://ikarus.webuntis.com/WebUntis/api/public/timetable/weekly/data"
 
-blocked_ids = []
+        querystring = {"elementType": self.element_type,"elementId": self.element_id,"date": self.date}
 
+        payload = ""
+        headers = {
+            "cookie": "schoolname="+schoolname_cookies+"; traceId=dbb69fa1e2dc8af353694414c2035671990c360d; JSESSIONID="+jsessionid_cookies+"",
+            "Accept": "application/json",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Referer": "https://ikarus.webuntis.com/WebUntis/?school="+self.schoolname+"",
+            "DNT": "1",
+            "Connection": "keep-alive",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin"
+        }
 
-with open("python_tests/test.json") as f:
-    data = json.load(f)
+        response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
 
-# for i, j in enumerate(data["data"]["result"]["data"]["elementPeriods"]["187"]):
-#     if j["hasPeriodText"]:
-#         try:
-#             print(j["periodText"])
-#         except KeyError:
-#             try: 
-#                 print(j["substText"])
-#             except KeyError:
-#                 pass
-#         print(j["startTime"])
-#         print(j["endTime"])
-#         for k, l in enumerate(j["elements"]):
-#             for m, n in enumerate(data["data"]["result"]["data"]["elements"]):
-#                 if n["id"] == l["id"]:
-#                     print(n["name"])
-#                     print(n["longName"])
-#         print()
-#         print()
+        with open("python_tests/test_"+self.element_id+".json", "w") as f:
+            f.write(response.text)
+
+        ###
+
+        blocked_ids = []
 
 
-date_list = [[],[],[],[],[]]
+        
+        with open("python_tests/test_"+self.element_id+".json", "r") as f:
+            data = json.load(f)
 
-for i,j in enumerate(data["data"]["result"]["data"]["elementPeriods"]["187"]):
-    untis_date = str(j["date"])
-    untis_start_time = str(j["startTime"])
-    if len(untis_start_time) == 3:
-        untis_start_time = "0" + untis_start_time
+
+        date_list = [[],[],[],[],[]]
+
+        for i,j in enumerate(data["data"]["result"]["data"]["elementPeriods"][self.element_id]):
+            untis_date = str(j["date"])
+            untis_start_time = str(j["startTime"])
+            if len(untis_start_time) == 3:
+                untis_start_time = "0" + untis_start_time
+            
+            date = datetime.datetime(year=int(untis_date[0:4]), month=int(untis_date[4:6]), day=int(untis_date[6:8]), hour=int(untis_start_time[0:2]), minute=int(untis_start_time[2:4]))
+            j["datetime_start"] = str(date)
+            date_list[date.weekday()].append(j)
+
+
+        for i, j in enumerate(date_list):
+            j.sort(key=lambda x: x["datetime_start"])
+
+        for i, j in enumerate(date_list):
+            temp_list = []
+            for k, g in itertools.groupby(j, key=lambda x: x["startTime"]):
+                temp_list.append(list(g))
+            date_list[i] = temp_list
+            print(i)
+
+            
+        start_times = []    
+        for i, j in enumerate(date_list):
+            for k, l in enumerate(j):
+                if l[0]["startTime"] not in start_times:
+                    start_times.append(l[0]["startTime"])
+        start_times.sort()
+        print(json.dumps(start_times, indent=4))
+
+        new_date_list = [[],[],[],[],[]]
+
+        for i, j in enumerate(new_date_list):
+            new_date_list[i] = [[] for k in range(len(start_times))]
+
+        for i, j in enumerate(date_list):
+            for k, l in enumerate(j):
+                for m, n in enumerate(start_times):
+                    if l[0]["startTime"] == n:
+                        new_date_list[i][m] = l
+
+        date_list = new_date_list
+            
+
+        with open("python_tests/test_output_"+self.element_id+".json", "w") as f:
+            f.write(json.dumps(date_list, indent=4))
+            
+        return None
     
-    date = datetime.datetime(year=int(untis_date[0:4]), month=int(untis_date[4:6]), day=int(untis_date[6:8]), hour=int(untis_start_time[0:2]), minute=int(untis_start_time[2:4]))
-    j["datetime_start"] = str(date)
-    date_list[date.weekday()].append(j)
 
-
-for i, j in enumerate(date_list):
-    j.sort(key=lambda x: x["datetime_start"])
-
-for i, j in enumerate(date_list):
-    temp_list = []
-    for k, g in itertools.groupby(j, key=lambda x: x["startTime"]):
-        temp_list.append(list(g))
-    date_list[i] = temp_list
-    print(i)
-
-    
-start_times = []    
-for i, j in enumerate(date_list):
-    for k, l in enumerate(j):
-        if l[0]["startTime"] not in start_times:
-            start_times.append(l[0]["startTime"])
-start_times.sort()
-print(json.dumps(start_times, indent=4))
-
-new_date_list = [[],[],[],[],[]]
-
-for i, j in enumerate(new_date_list):
-    new_date_list[i] = [[] for k in range(len(start_times))]
-
-for i, j in enumerate(date_list):
-    for k, l in enumerate(j):
-        for m, n in enumerate(start_times):
-            if l[0]["startTime"] == n:
-                new_date_list[i][m] = l
-
-date_list = new_date_list
-    
-
-with open("python_tests/test_output.json", "w") as f:
-    f.write(json.dumps(date_list, indent=4))
+if __name__ == "__main__":
+    WebUntis_request(1, 475, "2023-01-30", "hh5864").API_call()
       
